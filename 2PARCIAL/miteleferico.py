9c9f484886f59786e696c803e5ba833ab5f357ab
Yuri-Anahuaya <yu.890eduard546@gmail.com>
2025-11-17 09:31:08 -0400

    Update miteleferico.py

--- 2PARCIAL/miteleferico.py
+++ 2PARCIAL/miteleferico.py
@@ -1,8 +1,25 @@
+from linea import Linea
+
 class MiTeleferico:
-    def __init__():
-        linea = self.linea
-        cant = self.cantidad_ingresos
-    def MiTeleferico ():
-        
-    def AgregarPersonaFila(persona, linea):
-    def AgregarCabina(linea):
+    def __init__(self):
+        self.lineas = []  # lista de Linea
+
+    def agregarLinea(self, linea):
+        self.lineas.append(linea)
+
+    # d) mostrar línea con mayor ingreso SOLO con tarifa regular
+    def lineaMayorIngresoRegular(self):
+        mayor = None
+        mayor_ingreso = 0
+
+        for linea in self.lineas:
+            ingreso = 0
+            for cab in linea.cabinas:
+                for p in cab.personasAbordo:
+                    if 25 <= p.edad <= 60:
+                        ingreso += 3
+            if ingreso > mayor_ingreso:
+                mayor_ingreso = ingreso
+                mayor = linea
+
+        return mayor, mayor_ingreso
