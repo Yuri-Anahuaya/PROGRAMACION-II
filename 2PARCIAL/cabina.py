e4a5bf79d12714f13c3f0e5d5606a66ba6105b42
Yuri-Anahuaya <yu.890eduard546@gmail.com>
2025-11-17 09:24:58 -0400

    Add files via upload

---
+++ 2PARCIAL/cabina.py
@@ -0,0 +1,10 @@
+class Cabina:
+    def __init__():
+        linea = self.linea
+        cant = self.cantidad_ingresos
+    def MiTeleferico ():
+        
+    def AgregarPersonaFila(persona, linea):
+    def AgregarCabina(linea):
+
+
---
+++ 2PARCIAL/miteleferico.py
@@ -0,0 +1,8 @@
+class MiTeleferico:
+    def __init__():
+        linea = self.linea
+        cant = self.cantidad_ingresos
+    def MiTeleferico ():
+        
+    def AgregarPersonaFila(persona, linea):
+    def AgregarCabina(linea):
